1fb32d7af6d3dfc39bf90ca25d5b6331b931d075
E P <sseiei001@gmail.com>
2018-10-13 15:06:51 -0500

    adding twillio usage demo

---
+++ Twillio.py
@@ -0,0 +1,78 @@
+from functools import wraps
+
+from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.request_validator import RequestValidator
+
+from flask import (
+    Flask,
+    abort,
+    current_app,
+    request,
+)
+
+import os
+
+
+app = Flask(__name__)
+
+
+def validate_twilio_request(f):
+    """Validates that incoming requests genuinely originated from Twilio"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Create an instance of the RequestValidator class
+        validator = RequestValidator(os.environ.get('TWILIO_AUTH_TOKEN'))
+
+        # Validate the request using its URL, POST data,
+        # and X-TWILIO-SIGNATURE header
+        request_valid = validator.validate(
+            request.url,
+            request.form,
+            request.headers.get('X-TWILIO-SIGNATURE', ''))
+
+        # Continue processing the request if it's valid, return a 403 error if
+        # it's not
+        if request_valid or current_app.debug:
+            return f(*args, **kwargs)
+        else:
+            return abort(403)
+    return decorated_function
+
+
+@app.route('/voice', methods=['POST'])
+@validate_twilio_request
+def incoming_call():
+    """Twilio Voice URL - receives incoming calls from Twilio"""
+    # Create a new TwiML response
+    resp = VoiceResponse()
+
+    # <Say> a message to the caller
+    from_number = request.form['From']
+    body = """
+    Thanks for calling!
+    Your phone number is {0}. I got your call because of Twilio's webhook.
+    Goodbye!""".format(' '.join(from_number))
+    resp.say(body)
+
+    # Return the TwiML
+    return str(resp)
+
+
+@app.route('/message', methods=['POST'])
+@validate_twilio_request
+def incoming_message():
+    """Twilio Messaging URL - receives incoming messages from Twilio"""
+    # Create a new TwiML response
+    resp = MessagingResponse()
+
+    # <Message> a text back to the person who texted us
+    body = "Your text to me was {0} characters long. Webhooks are neat :)" \
+        .format(len(request.values['Body']))
+    resp.message(body)
+
+    # Return the TwiML
+    return str(resp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
