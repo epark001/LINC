78a518ae521bbcbb218e78cd1bac4774f25c4186
E P <sseiei001@gmail.com>
2018-10-14 10:22:35 -0500

    merged twilio with linc through text messages

--- Core.py
+++ Core.py
@@ -6,74 +6,76 @@ from twilio.twiml.voice_response import VoiceResponse
 import json
 import requests
 import os
-import MySQLdb
+#import datetime
+import logging
 
-CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
-CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
-CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
-clincHost = os.environ.get('CLINC_HOST')
-clincVer = os.environ.get('CLINC_VER')
-account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+from bson import json_util
+from datetime import datetime, timedelta, timezone
+#import unix_socket
+#from flask_sqlalchemy import SQLAlchemy
+#import sqlalchemy
+#import MySQLdb
+
+CLOUDSQL_CONNECTION_NAME = os.environ.get("CLOUDSQL_CONNECTION_NAME")
+CLOUDSQL_USER = os.environ.get("CLOUDSQL_USER")
+CLOUDSQL_PASSWORD = os.environ.get("CLOUDSQL_PASSWORD")
+clincHost = os.environ.get("CLINC_HOST")
+clincVer = os.environ.get("CLINC_VER")
+#account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+account_sid = "ACce3d35b11990b0a5f86d17795ae7ffe6"
+#auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+auth_token = "33808ddf18ac53ba89ffff98fbf2c460"
+
+#app1.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
+#app1.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+#db = SQLAlchemy(app1)
 
 
 app = Flask(__name__)
 
-@app.route('/connectSQL', methods=['GET'])
-def connect_to_cloudsql():
-    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
-    # will be set to 'Google App Engine/version'.
-    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
-        # Connect using the unix socket located at
-        # /cloudsql/cloudsql-connection-name.
-        cloudsql_unix_socket = os.path.join(
-            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
-
-        db = MySQLdb.connect(
-            unix_socket=cloudsql_unix_socket,
-            user=CLOUDSQL_USER,
-            passwd=CLOUDSQL_PASSWORD)
-
-    # If the unix socket is unavailable, then try to connect using TCP. This
-    # will work if you're running a local MySQL server or using the Cloud SQL
-    # proxy, for example:
-    #
-    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
-    #
-    else:
-        db = MySQLdb.connect(
-            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
-    return db
-
-@app.route('/fetchSQLvars', methods=['GET'])
-def fetchSQLvars(self):
-    #"""Simple request handler that shows all of the MySQL variables."""
-    self.response.headers['Content-Type'] = 'text/plain'
-
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    cursor.execute('SHOW VARIABLES')
-
-    for r in cursor.fetchall():
-        self.response.write('{}\n'.format(r))
-
-def queryClinc(inputQuery):
+def queryClinc(inputQuery, phoneNum):
+    #url = "https://"+hostname+"/"+version+"/query"
+    url = "https://mhacks.clinc.ai/v1/query"
     headers = {
         "Authorization": authClinc(),
         "Content-Type":"application/json"
     }
     payload = {
-        'query': inputQuery,
-        'lat': 42.2730207,
-        'lon': -83.7517747,
-        'time_offset': 300,
-        'device': 'Mozilla/5.0'
+        "query": inputQuery,
+        "lat": 42.2730207,
+        "lon": -83.7517747,
+        "time_offset": 300,
+        "device": "Mozilla/5.0"
     }
-    url = "https://"+hostname+"/"+version+"/query"
-    output = requests.post(url2, data = json.dumps(payload), headers = headers)
+    text_file = open("sessionLogs.txt","r")
+    logs = json.loads(text_file.read(), object_hook=json_util.object_hook)
+    text_file.close()
+    if phoneNum in logs:
+        if logs[phoneNum]["lastSeen"]+timedelta(hours=1) > datetime.now(tz=timezone.utc):
+            payload["dialog"] = logs[phoneNum]["dialog"]
+    else:
+        logs[phoneNum] = {}
+    output = requests.post(url, data = json.dumps(payload), headers = headers)
     output = json.loads(output.text)
-    response = output['visuals']['speakableResponse']
-    return reponse
+    #print(output["dialog"])
+    # logs[phoneNum] = {
+    #     "dialogue": output["dialogue"],
+    #     "lastSeen": timedelta.now()
+    # }
+    logs[phoneNum]["dialog"] = output["dialog"]
+    logs[phoneNum]["lastSeen"] = datetime.now(tz=timezone.utc)
+
+    print(logs)
+
+    raw = open("sessionLogs.txt", "w")
+    # contents = json.loads(raw.read())
+    # raw.seek(0)
+    # raw.truncate()
+    raw.write(json.dumps(logs, default=json_util.default))
+    raw.close()
+
+    return output["visuals"]["speakableResponse"]
 
 def authClinc():
     authForm = {
@@ -84,44 +86,58 @@ def authClinc():
         "grant_type":"password",
         "institution":"mhacks2"
     }
+    url = "https://mhacks.clinc.ai/v1/oauth"
 
-    url = "https://"+hostname+"/"+version+"/oauth"
-    output = requests.post(url, data = form )
+    #url = "https://"+hostname+"/"+version+"/oauth"
+    output = requests.post(url, data = authForm )
     output = json.loads(output.text)
-    return "Bearer "+output['access_token']
+    return "Bearer "+output["access_token"]
 
-@app.route('/webhook', methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    if request.method == 'POST':
+    if request.method == "POST":
         client = Client(account_sid, auth_token)
         message_history = client.messages.list()
         for message in client.messages.list():
-            print (message.body);
-            print (message.from_);
+            #print(message.body);
+            textBody = message.body;
+            #print(message.from_);
+            phoneNum = message.from_;
             break
+
+        output = queryClinc(textBody, phoneNum)
+
         message = client.messages \
             .create(
-                body="Hi! Message received",
-                from_='+19525294321',
-                to='+13476269937'
+                body=output,
+                from_="+19525294321",
+                to=phoneNum
                 )
-        return '', 200
+        return "", 200
     else:
         abort(400)
 
-@app.route("/calls", methods=['GET', 'POST'])
+@app.route("/calls", methods=["GET", "POST"])
 def record():
     """Returns TwiML which prompts the caller to record a message"""
     # Start our TwiML response
     response = VoiceResponse()
     # Use <Say> to give the caller some instructions
-    response.say('Hello. Please leave a message after the beep.')
-    # Use <Record> to record the caller's message
+    response.say("Hello. Please leave a message after the beep.")
+    # Use <Record> to record the caller"s message
     response.record()
     # End the call with <Hangup>
     response.hangup()
     return str(response)
 
 
-if __name__ == '__main__':
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception("An error occurred during a request.")
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+if __name__ == "__main__":
     app.run()
