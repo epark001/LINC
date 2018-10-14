39a12876a1c1525674bf9f57b42e57a801707164
E P <sseiei001@gmail.com>
2018-10-14 10:59:28 -0500

    pushing new files for tranferal

--- Core.py
+++ Core.py
@@ -43,33 +43,25 @@ app = Flask(__name__)
 
 @app.route("/calls", methods=['GET', 'POST'])
 def record():
-    client = Client(account_sid, auth_token)
-    # Start our TwiML response
-    response = VoiceResponse()
-    # Use <Say> to give the caller some instructions
-    response.say('Hello this is LINC. What would you like to talk about?')
-    # Use <Record> to record the caller's message
-    response.record(transcribe=True, playBeep=True,timeout=4)
-    # response.pause(length=3)
-    client = Client(account_sid, auth_token)
-    trans_list = client.transcriptions.list()
-    for transcription in trans_list:
-       latest_transcript = transcription.transcription_text
-       status= transcription.status
-       if(latest_transcript!=None):
-           unicodedata.normalize('NFKD', latest_transcript).encode('ascii','ignore')
-           print(status);
-           print(transcription.sid);
-           print(latest_transcript);
-           transcription.delete();
-           break
-       else:
-           response.say("I'm sorry. I didn't get that. Can you repeat it?")
-    print(latest_transcript != "hello")
-    if (latest_transcript != "hello"):
-       print("boolean worked");
-       response.redirect("http://4b930e15.ngrok.io/voice_response")
-    response.hangup()
+    unicodedata.normalize('NFKD', latest_transcript).encode('ascii','ignore')
+    while (latest_trancript != "Goodbye"):
+        client = Client(account_sid, auth_token)
+        # Start our TwiML response
+        response = VoiceResponse()
+        response.say('Please say goodbye')
+        response.record(transcribe=True, playBeep=True,timeout=4)
+        # Use <Say> to give the caller some instructions
+        response.say('Hello this is reponse. What would you like to talk about?')
+        # Use <Record> to record the caller's message
+        response.record(transcribe=True, playBeep=True,timeout=4)
+        # response.pause(length=3)
+        client = Client(account_sid, auth_token)
+        trans_list = client.transcriptions.list()
+        for transcription in trans_list:
+            latest_transcript = transcription.transcription_text
+            status= transcription.status
+    response.say('Please say goodbye')
+    response.hangup();
     return str(response)
 
 def queryClinc(inputQuery, phoneNum):
